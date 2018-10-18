0f7a073368eec32384c11855d2d77890439f23ab
jianghaifei <1465089870@qq.com>
2018-10-18 09:23:44 +0800

    上传

--- src/main/resources/static/js/jiami/mode-ecb.js
+++ src/main/resources/static/js/jiami/mode-ecb.js
@@ -24,3 +24,19 @@ CryptoJS.mode.ECB = (function () {
 
     return ECB;
 }());
+
+//加密
+function encrypt(word){
+    var key = CryptoJS.enc.Utf8.parse("13223wrwe4345678");//秘钥
+
+    var srcs = CryptoJS.enc.Utf8.parse(word);
+    var encrypted = CryptoJS.AES.encrypt(srcs, key, {mode:CryptoJS.mode.ECB,padding: CryptoJS.pad.Pkcs7});
+    return encrypted.toString();
+}
+//解密
+function decrypt(word){
+    var key = CryptoJS.enc.Utf8.parse("13223wrwe4345678");  //秘钥
+
+    var decrypt = CryptoJS.AES.decrypt(word, key, {mode:CryptoJS.mode.ECB,padding: CryptoJS.pad.Pkcs7});
+    return CryptoJS.enc.Utf8.stringify(decrypt).toString();
+}
